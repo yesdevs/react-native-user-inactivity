36a50abdac3b5131c7f8ba9fbbfdfff3371e558a
Alejandro Berbin <alejandro.berbin@yesdevs.com>
2025-06-12 17:48:53 +0200

    chore: update version to 1.3.0 and upgrade dependencies

--- lib/useTimeout/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { useTimeoutDefault as default } from './useTimeoutDefault';
-export { useTimeout } from './useTimeout';
-export { TimeoutHandler } from './TimeoutHandler';
-export { defaultTimeoutHandler } from './defaultTimeoutHandler';
